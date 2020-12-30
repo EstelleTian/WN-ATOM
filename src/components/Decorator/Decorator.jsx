4154b5452a29dc49805b94306d86b9c3bbcb21ce
zhangxuyou <zxydev@163.coom>
2020-12-30 20:30:55 +0800

    模块组件调整

---
+++ src/components/Decorator/Decorator.jsx
@@ -0,0 +1,41 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-15 15:54:57
+ * @LastEditTime: 2020-12-21 09:42:52
+ * @LastEditors: Please set LastEditors
+ * @Description: 航班查询
+ * @FilePath: \WN-CDM\src\components\FlightSearch\FlightSearch.jsx
+ */
+import React from 'react'
+import './Decorator.scss'
+
+
+//装饰模块
+const Decorator = (props) => {
+
+
+    return (
+
+        <div className="decorator">
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="10">/</p>
+            <p className="slash" data-opacity="9">/</p>
+            <p className="slash" data-opacity="8">/</p>
+            <p className="slash" data-opacity="7">/</p>
+            <p className="slash" data-opacity="6">/</p>
+            <p className="slash" data-opacity="5">/</p>
+            <p className="slash" data-opacity="4">/</p>
+            <p className="slash" data-opacity="3">/</p>
+            <p className="slash" data-opacity="2">/</p>
+            <p className="slash" data-opacity="1">/</p>
+        </div>
+    )
+}
+
+
+export default Decorator
