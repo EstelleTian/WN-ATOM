8c736c888840784727f6c743604fa0eeaac3e066
zhangxuyou <zxydev@163.coom>
2021-01-08 16:33:21 +0800

    正常率数值格式化

--- src/components/ExecuteKPI/PreDelay.jsx
+++ src/components/ExecuteKPI/PreDelay.jsx
@@ -10,7 +10,7 @@ function PreDelay(props){
     let { entiretyNormalRate, insideApNormalRate ={ }} = KPIData;
 
     if(isValidVariable(entiretyNormalRate)){
-        entiretyNormalRate = (entiretyNormalRate*1) < 0 ? "N/A" : entiretyNormalRate;
+        entiretyNormalRate = (entiretyNormalRate*1) < 0 ? "N/A" : entiretyNormalRate*100;
     }else{
         entiretyNormalRate = "N/A";
     }
@@ -28,6 +28,65 @@ function PreDelay(props){
     }
 
     let list = getApNormalRateList(insideApNormalRate);
+
+    /**
+     * 格式化百分比数值
+     * */
+    const formatPercent =(percent) => {
+        if(percent ==="N/A"){
+            return `N/A%`
+        }else if(percent === '100' ){
+            // 处理100%数值显示(原组件会显示成对号)
+            return `100%`
+        } else {
+            return `${percent}%`
+        }
+
+    }
+
+
+    /**
+     * 转换百分比
+     * */
+    const converPercent = (percent) => {
+
+        if(isValidVariable(percent)){
+            // 若值为'N/A'返回0
+            if(percent === 'N/A'){
+                return  0
+            }else {
+                // 小于0的返回0, 其他情况返回原值*100
+                return (percent*1) < 0 ? 0 : (percent *100)
+            }
+
+        }else {
+            // 若无效则返回0
+            return 0
+        }
+    }
+
+    /**
+     * 转换百分比显示
+     *
+     * */
+    const converPercentText = (percent) => {
+
+        if(isValidVariable(percent)){
+            // 若值为'N/A'返回0
+            if(percent === 'N/A'){
+                return  'N/A'
+            }else {
+                // 小于0的返回0, 其他情况返回原值*100
+                return (percent*1) < 0 ? 'N/A'  : (percent *100)
+            }
+
+        }else {
+            // 若无效则返回'N/A'
+            return "N/A"
+        }
+    }
+
+
     list = list.sort((item1,item2)=>{
         let nameA = item1.key.toUpperCase(); // ignore upper and lowercase
         let nameB = item2.key.toUpperCase(); // ignore upper and lowercase
@@ -51,8 +110,9 @@ function PreDelay(props){
                     <Progress
                         strokeLinecap="square"
                         type="dashboard"
-                        percent={entiretyNormalRate*100}
+                        percent={entiretyNormalRate}
                         // strokeWidth={8}
+                        format={formatPercent}
                         gapDegree={1}
                         trailColor="#64737a"
                     />
@@ -62,10 +122,10 @@ function PreDelay(props){
                     {
                         list.map((item, index) => (
                                 <Col span={12} className="block row_model flex" key={ item.key }>
-                                    <div className="block-title percent text-center">{(item.value*1) < 0 ? "N/A" : (item.value*100)}%</div>
+                                    <div className="block-title percent text-center">{`${converPercentText(item.value)}%`}</div>
                                     <div className="flex justify-content-center layout-column">
                                         <Progress
-                                            percent={(item.value*1) < 0 ? 0 : (item.value *100)}
+                                            percent={ converPercent(item.value) }
                                             showInfo={false}
                                             trailColor="#64737a"
                                         />
