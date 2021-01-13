935983d46fb89b1f5a5694bde65873bad858c1d1
zhangxuyou <zxydev@163.coom>
2021-01-13 20:49:41 +0800

    流控创建页面样式调整

--- src/pages/CreateFlowPage/CreateFlowPage.jsx
+++ src/pages/CreateFlowPage/CreateFlowPage.jsx
@@ -9,7 +9,7 @@ function CreateFlowPage(props){
     return (
         <div className="create-flow-container">
             <div className="content">
-                <RestrictionForm pageType="CREATE" showImportBtn={true} btnName="创建方案" width={1280}  />
+                <RestrictionForm pageType="CREATE" showImportBtn={true} btnName="创建方案" />
             </div>
 
         </div>
