e1b1f1fc92297f3dd31b235187077aa68b8c037e
zhangxuyou <zxydev@163.coom>
2021-01-18 13:23:59 +0800

    总体态势监控页面添加容流略图模块stortes数据

---
+++ src/stores/capacityFlowMonitorStores.jsx
@@ -0,0 +1,39 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-14 10:18:25
+ * @LastEditTime: 2020-12-22 19:24:44
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \WN-CDM\src\stores\FlightStores.jsx
+ */
+
+import { makeObservable, observable, action, computed } from 'mobx'
+
+// 单个方案执行KPI数据
+class CapacityFlowMonitorData {
+    constructor(){
+        makeObservable(this)
+    }
+    // 列表
+    @observable monitorData = {};
+    //数据时间
+    @observable generateTime = "";
+    //数据获取
+    @observable loading = false;
+    //定时器
+    @observable timeoutId = "";
+
+    //更新航班数据
+    @action updateCapacityFlowMonitorData( monitorData ){
+        this.monitorData = monitorData;
+    }
+    //更新loading状态
+    @action toggleLoad( load ){
+        this.loading = load;
+    }
+}
+
+let capacityFlowMonitorData = new CapacityFlowMonitorData();
+
+
+export { capacityFlowMonitorData }
