3d1e28e8706584c0083d9ea8731890f80262d844
username <liutj@adcc.com.cn>
2021-01-20 14:48:01 +0800

    登录样式

--- src/components/SchemeList/SchemeItem.jsx
+++ src/components/SchemeList/SchemeItem.jsx
@@ -92,6 +92,8 @@ function SchemeItem(props){
             });
             setWindow(newWindow);
             setWindowClass(windowClass);
+        }else{
+            window.show();
         }
     }
 
