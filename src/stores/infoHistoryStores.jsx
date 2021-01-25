eaa1d4699a3c72c1445d56c615cf8a6aa5f36845
zhangxuyou <zxydev@163.coom>
2021-01-25 17:27:48 +0800

    消息历史页面添加

---
+++ src/stores/infoHistoryStores.jsx
@@ -0,0 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-14 10:18:25
+ * @LastEditTime: 2021-01-21 15:07:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \WN-CDM\src\stores\schemeStores.jsx
+ */
+import { makeObservable, observable, action, computed } from 'mobx'
+import { isValidVariable } from 'utils/basic-verify'
+ // 消息历史流列表数据
+class InfoHistoryData{
+    constructor(){
+        makeObservable(this)
+    }
+    // 消息历史列表
+    @observable infoData = {};
+    //数据时间
+    @observable generateTime = "";
+    //定时器
+    @observable timeoutId = "";
+    //导航选中的过滤类型项
+    @observable filterType= 'ALL';
+    //数据获取
+    @observable loading = false;
+    //更新表格loading状态
+    @action toggleLoad( load ){
+        this.loading = load;
+    }
+    //更新列表过滤类型项
+    @action setFilterType( value ){
+        this.filterType = value;
+    }
+
+    // 更新消息历史数据-全部
+    @action updateTasks( tasks, generateTime){
+        this.infoData = tasks;
+        this.generateTime = generateTime;
+    }
+}
+
+let infoHistoryData = new InfoHistoryData();
+
+export { infoHistoryData }
