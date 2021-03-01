4477b6a4a4730d48474d45b0fe0d5ef852542e74
username <liutj@adcc.com.cn>
2021-03-01 13:55:16 +0800

    444

---
+++ expressPro.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-24 16:50:48
+ * @LastEditTime: 2021-02-24 16:54:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \WN-ATOM\expressPro.js
+ */
+
+var express = require('express');
+var fs=require("fs");
+var app = express();
+
+//方法1：通过express.static访问静态文件，这里访问的是ajax.html
+// app.use(express.static("./"));
+
+//方法2：使用fs.readFile打开html文件
+app.get("./build/index.html", function(request, response) {
+   fs.readFile("./"+request.path.substr(1),function(err,data){
+        // body
+        if(err){
+            console.log(err);
+            //404：NOT FOUND
+            response.writeHead(404,{"Content-Type":"text/html"});
+        }
+        else{
+            //200：OK
+            response.writeHead(200,{"Content-Type":"text/html"});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+});
+
+app.listen(5555, function() {   //监听http://127.0.0.1:3000端口
+    console.log("server start");
+});
