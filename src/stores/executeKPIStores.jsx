7b68a41365e40e84bb702860a61dc79cf8b99994
zhangxuyou <zxydev@163.com>
2021-04-19 16:13:08 +0800

    KPI store 结构调整

--- src/stores/executeKPIStores.jsx
+++ src/stores/executeKPIStores.jsx
@@ -1,11 +1,4 @@
-/*
- * @Author: your name
- * @Date: 2020-12-14 10:18:25
- * @LastEditTime: 2021-03-05 10:07:02
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \WN-CDM\src\stores\FlightStores.jsx
- */
+
 
 import { makeObservable, observable, action, computed } from 'mobx'
 
@@ -16,22 +9,119 @@ class ExecuteKPIData {
         makeObservable(this)
     }
     // 数据集合
-    @observable executeData = {};
+    @observable KPIData = {};
     //数据时间
     @observable generateTime = "";
     //数据获取
     @observable loading = false;
     //定时器
     @observable timeoutId = "";
+    
+    //航班列表模态框显示状态
+    @observable flightListModalVisible = false;
 
-    //更新航班数据
+    //航班列表类别
+    @observable flightListCategory = "";
+    //航班列表类别
+    @observable flightListCategoryZh = "";
+    
+    //更新KPI数据
     @action updateExecuteKPIData( KPIData ){
-        this.executeData = KPIData;
+        this.KPIData = KPIData;
     }
     //更新loading状态
     @action toggleLoad( load ){
         this.loading = load;
     }
+    //更新航班列表模态框显示状态
+    @action toggleFlightListModalVisible( visible ){
+        this.flightListModalVisible = visible;
+    }
+    //更新航班列表类别
+    @action updateFlightListCategory ( category ){
+        this.flightListCategory = category;
+    }
+    //更新航班列表类别
+    @action updateFlightListCategoryZh ( categoryZh ){
+        this.flightListCategoryZh = categoryZh;
+    }
+
+    //更新航班列表类别
+    @computed get getFocusCategoryFlight ( ){
+        debugger
+        return this[this.flightListCategory] || [];
+    }
+
+
+
+    // 影响航班数据集合
+    @computed get impactFlight() {
+        const {impactFlightCount={}} = this.KPIData;
+        const { AF}= impactFlightCount;
+        return AF
+    }
+    // 已执行航班集合
+    @computed get executedFlight() {
+        const {executedFlight} = this.KPIData;
+        return executedFlight
+    }
+    // 执行中航班集合
+    @computed get inExecutionFlight() {
+        const {inExecutionFlight} = this.KPIData;
+        return inExecutionFlight
+    }
+    // 待放行航班集合
+    @computed get nonExecutionFlight() {
+        const {nonExecutionFlight} = this.KPIData;
+        return nonExecutionFlight
+    }
+
+    // 影响等级
+    @computed get impactDegree() {
+        const {impactDegree} = this.KPIData;
+        return impactDegree
+    }
+    // 全区DCB
+    @computed get tacticDCB() {
+        const {tacticDCB} = this.KPIData;
+        return tacticDCB
+    }
+    // 区内DCB
+    @computed get inDCB() {
+        const {inDCB} = this.KPIData;
+        return inDCB
+    }
+    // 区外DCB
+    @computed get outDCB() {
+        const {outDCB} = this.KPIData;
+        return outDCB
+    }
+    // 比率数据对象
+    @computed get degree() {
+        const {degree} = this.KPIData;
+        return degree
+    }
+    // 预计起飞正常率  
+    @computed get entiretyNormalRate() {
+        const {entiretyNormalRate} = this.KPIData;
+        return entiretyNormalRate
+    }
+    // 实际起飞正常率
+    @computed get entiretyDepNormalRate() {
+        const {entiretyDepNormalRate} = this.KPIData;
+        return entiretyDepNormalRate
+    }
+    // 四大机场预计起飞正常率
+    @computed get apEstimateRateMap() {
+        const {apEstimateRateMap} = this.KPIData;
+        return apEstimateRateMap
+    }
+    // 四大机场实际起飞正常率
+    @computed get apDepEstimateRateMap() {
+        const {apDepEstimateRateMap} = this.KPIData;
+        return apDepEstimateRateMap
+    }
+
 }
 
 // 单个方案执行KPI数据部分2
@@ -56,8 +146,6 @@ class PerformanceKPIData {
         this.loading = load;
     }
 }
-
-
 
 let executeKPIData = new ExecuteKPIData();
 let performanceKPIData = new PerformanceKPIData();
