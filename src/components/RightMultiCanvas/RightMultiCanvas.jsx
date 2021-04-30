ab6fbb9684ae2ea1503fe1adcc39206cf3ad5178
liutianjiao <liutj@adcc.com.cn>
2021-04-30 08:30:37 +0800

    跑道与方案列表不互切

--- src/components/RightMultiCanvas/RightMultiCanvas.jsx
+++ src/components/RightMultiCanvas/RightMultiCanvas.jsx
@@ -1,76 +1,87 @@
 /*
  * @Author: liutianjiao
  * @Date: 2021-01-07 20:35:06
- * @LastEditTime: 2021-02-25 13:29:37
+ * @LastEditTime: 2021-04-29 16:40:01
  * @LastEditors: Please set LastEditors
  * @Description: 左上切换模块 执行kpi 豁免航班 等待池 特殊航班 失效航班 待办事项
  * @FilePath: \WN-ATOM\src\components\RightMultiCanvas\RightMultiCanvas.jsx
  */
-import React, { lazy, Suspense} from 'react';
-import {inject, observer} from "mobx-react";
-import {Empty, Spin} from "antd";
-import ModalBox from 'components/ModalBox/ModalBox'
-import { formatTimeString } from 'utils/basic-verify'
-import './RightMultiCanvas.scss'
+import React, { lazy, Suspense } from "react";
+import { inject, observer } from "mobx-react";
+import { Empty, Spin } from "antd";
+import ModalBox from "components/ModalBox/ModalBox";
+import { formatTimeString } from "utils/basic-verify";
+import "./RightMultiCanvas.scss";
 
 //TODO 真实数据，用于公司环境
-const SchemeListModal = lazy(() => import('components/SchemeList/SchemeList') );
+const SchemeListModal = lazy(() => import("components/SchemeList/SchemeList"));
 //TODO 本地模拟数据，用于无接口调试样式
 // const SchemeList = lazy(() => import('components/SchemeList/SchemeListMock') );
-const RunwayList = lazy(() => import('components/Runway/RunwayList') );
+const RunwayList = lazy(() => import("components/Runway/RunwayList"));
 
-
-function RightMultiCanvas(props){
-    const { systemPage } = props;
-    const { rightActiveName } = systemPage;
-    return (
-        <div className="cont_right">
-            {
-                rightActiveName === "scheme"
-                ? <ModalBox
-                        // title="方案列表"
-                        title={`方案列表 (数据时间:${ formatTimeString(props.schemeListData.generateTime) })`}
-                        showDecorator = {true}
-                    >
-                    <Suspense fallback={<div className="load_spin"><Spin tip="加载中..."/></div>}>
-                        <SchemeListModal />
-                    </Suspense>
-
-                    </ModalBox>
-                    : ""
+function RightMultiCanvas(props) {
+  const { systemPage } = props;
+  const { rightActiveName } = systemPage;
+  return (
+    <div className="cont_right">
+      {rightActiveName === "scheme" || rightActiveName === "runway" ? (
+        <ModalBox
+          // title="方案列表"
+          title={`方案列表 (数据时间:${formatTimeString(
+            props.schemeListData.generateTime
+          )})`}
+          showDecorator={true}
+        >
+          <Suspense
+            fallback={
+              <div className="load_spin">
+                <Spin tip="加载中..." />
+              </div>
             }
-            {
-                rightActiveName === "outer_scheme"
-                    ? <ModalBox
-                        title="外部流控列表"
-                        showDecorator = {true}
-                    >
-
-                        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} imageStyle={{ color:"#fff"}} />
-                    </ModalBox>
-                    : ""
+          >
+            <SchemeListModal />
+          </Suspense>
+        </ModalBox>
+      ) : (
+        ""
+      )}
+      {rightActiveName === "outer_scheme" ? (
+        <ModalBox title="外部流控列表" showDecorator={true}>
+          <Empty
+            image={Empty.PRESENTED_IMAGE_SIMPLE}
+            imageStyle={{ color: "#fff" }}
+          />
+        </ModalBox>
+      ) : (
+        ""
+      )}
+      {rightActiveName === "runway" ? (
+        <ModalBox
+          // title="方案列表"
+          title={`跑道列表 (数据时间:${formatTimeString(
+            props.schemeListData.generateTime
+          )})`}
+          showDecorator={true}
+          className="runway_modal"
+        >
+          <Suspense
+            fallback={
+              <div className="load_spin">
+                <Spin tip="加载中..." />
+              </div>
             }
-            {
-                rightActiveName === "runway"
-                ? <ModalBox
-                        // title="方案列表"
-                        title={`跑道列表 (数据时间:${ formatTimeString(props.schemeListData.generateTime) })`}
-                        showDecorator = {true}
-                    >
-                    <Suspense fallback={<div className="load_spin"><Spin tip="加载中..."/></div>}>
-                        <RunwayList />
-                    </Suspense>
-
-                    </ModalBox>
-                    : ""
-            }
-        </div>
-
-    )
-
+          >
+            <RunwayList />
+          </Suspense>
+        </ModalBox>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
-export default inject("systemPage", "schemeListData")(observer(RightMultiCanvas))
-
-
-
+export default inject(
+  "systemPage",
+  "schemeListData"
+)(observer(RightMultiCanvas));
--- src/components/SchemeList/SchemeList.jsx
+++ src/components/SchemeList/SchemeList.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-10 11:08:04
- * @LastEditTime: 2021-04-23 09:22:30
+ * @LastEditTime: 2021-04-28 16:38:00
  * @LastEditTime: 2021-03-04 14:40:22
  * @LastEditors: Please set LastEditors
  * @Description: 方案列表
@@ -675,7 +675,7 @@ function SList(props) {
 
   //接收客户端传来方案id，用以自动切换到选中方案
   NWGlobal.setSchemeId = useCallback((schemeId, title) => {
-    alert("收到id:" + schemeId + "  title:" + title);
+    // alert("收到id:" + schemeId + "  title:" + title);
     //主动获取一次
     getSchemeList().then(function (data) {
       //验证有没有这个方案
@@ -804,7 +804,7 @@ function SList(props) {
         )}
         {visible ? (
           <SchemeModal
-            userId = {userId}
+            userId={userId}
             visible={visible}
             setVisible={setVisible}
             modalType={modalType}
