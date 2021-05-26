6aff30bd8beb11db9c1ec298af4d9eff23c7e2a9
liutianjiao <liutj@adcc.com.cn>
2021-05-26 10:45:14 +0800

    url提交

--- src/utils/request-urls-prod.js
+++ src/utils/request-urls-prod.js
@@ -1,7 +1,7 @@
 /*
  * @Author: liutianjiao
  * @Date:
- * @LastEditTime: 2021-05-24 13:20:42
+ * @LastEditTime: 2021-05-26 10:25:42
  * @LastEditors: Please set LastEditors
  * @Description: 生产环境-项目所有请求url
  * @FilePath: request-urls.js
@@ -10,6 +10,7 @@
 const workflowIP = "http://192.168.210.120:81";
 //MDRS(路武臣)
 const mdrsIP = "http://192.168.210.120:81";
+// const mdrsIP = "http://192.168.210.120:81"
 //消息订阅(路武臣)
 const userSubscribeIP = "http://192.168.210.120:81";
 //
@@ -17,31 +18,32 @@ const runwayIP = "http://192.168.210.120:81";
 const clearanceIP = "http://192.168.210.120:81";
 const schemeIP = "http://192.168.210.120:81";
 // 获取指定航路点对应的速度(路武臣)
-const speedIP = "http://192.168.210.120:81"
+const speedIP = "http://192.168.210.120:81";
 // 校验航路接口IP(李东骏)
-const validateRouteIP = "http://192.168.210.120:81"
+const validateRouteIP = "http://192.168.210.120:81";
+
 //协调ip
 const CollaborateIP = "http://192.168.210.120:81";
 //登录IP(姜浩)
 const loginIP = "http://192.168.194.21:18380";
 //容量管理IP(韩涛)
-const capacityIP = "http://192.168.210.120:81"
+const capacityIP = "http://192.168.210.120:81";
 //容流数据IP
-const capacityFlowIP= "http://192.168.210.120:81"
+const capacityFlowIP= "http://192.168.210.120:81";
 //容流气象数据IP
-const capacityFlowWeatherIP= "http://192.168.210.120:81"
+const capacityFlowWeatherIP= "http://192.168.210.120:81";
 //总体监控-协调KPI-航班协调-数据IP
-const totalCollaborateIP= "http://192.168.210.120:81"
+const totalCollaborateIP= "http://192.168.210.120:81";
 //总体监控-协调KPI-运行配置-数据IP
-const totalOperationIP= "http://192.168.210.120:81"
+const totalOperationIP= "http://192.168.210.120:81";
 //航班详情、略情查询IP(韩涛)
-const flightInfoIP= "http://192.168.210.120:81"
+const flightInfoIP= "http://192.168.210.120:81";
 //总体监控-执行KPI-IP(孔凡续)
-const executeKPIDataIP= "http://192.168.210.120:81"
+const executeKPIDataIP= "http://192.168.210.120:81";
 //航班执行数据-IP()
-const performanceDataIP= "http://192.168.210.120:81"
+const performanceDataIP= "http://192.168.210.120:81";
 // 获取用户订阅的容流监控单元数据-IP()
-const userSubscribeCapacityFlowMonitorUnitDataIP= "http://192.168.210.120:81"
+const userSubscribeCapacityFlowMonitorUnitDataIP= "http://192.168.210.120:81";
 //待办航班列表-放行监控页面中的-IP(张杰)
 const todoListIP= "http://192.168.210.120:81";
 //我的申请列表-放行监控页面中的-IP(张杰)
@@ -58,7 +60,7 @@ const ReqUrls = {
     //获取--方案列表
     schemeListUrl: clearanceIP+"/implementTactics",
     //获取--模拟方案详情
-    simulationSchemeDetailUrl:schemeIP+"/simulationTactics/id/",
+    simulationSchemeDetailUrl:schemeIP+"/scheme-flow-server/simulationTactics/id/",
     //获取--航班列表数据
     flightsDataUrl: clearanceIP+"/tactic/",
     //获取--航班列表数据(id为空)
@@ -70,7 +72,7 @@ const ReqUrls = {
     //获取航班执行数据
     performanceDataUrl: performanceDataIP+"/hydrogen-traffic-flow-performance-retrieval-server/monitor/nw/v1/flight/areaname",
     // 总体监控-获取限制数据
-    restrictionDataUrl: schemeIP+"/implementTactics/statistics",
+    restrictionDataUrl: schemeIP+"/scheme-flow-server/implementTactics/statistics",
     // 获取用户订阅的容流监控单元数据
     userSubscribeCapacityFlowMonitorUnitDataUrl: userSubscribeCapacityFlowMonitorUnitDataIP+"/user-template-rest/template/user/",
     // 获取容流数据
@@ -82,10 +84,11 @@ const ReqUrls = {
     //获取流控数据(ATOM变更)
     ATOMModifyDataUrl: schemeIP+"/scheme-flow-server/restrictions/modify/",
     //获取流控数据(NTFM新增)
-    NTFMCreateDataUrl: schemeIP+"/restrictions/createNtfm/",
+    //NTFMCreateDataUrl: schemeIP+"/restrictions/createNtfm/",
+    NTFMCreateDataUrl: schemeIP+"/scheme-flow-server/restrictions/createNtfm/",
 
     //获取流控数据(NTFM变更)
-    NTFMModifyDataUrl: schemeIP+"/restrictions/modifyNtfm/",
+    NTFMModifyDataUrl: schemeIP+"/scheme-flow-server/restrictions/modifyNtfm/",
     //创建方案(外区流控导入)-数据提交
     importSchemeUrl: schemeIP+"/scheme-flow-server/simulationTactics/import/sim/",
     // 创建和修改方案-数据提交
@@ -94,8 +97,9 @@ const ReqUrls = {
     speedUrl: speedIP+"/flow-route-point-speed-calculate-server/speed-retrive/speed/",
     // 校验航路接口
     validateRouteUrl: validateRouteIP+"/hydrogen_reroute_check_server/reroute/rerouteCheckPost",
+
     //修改模拟状态的方案-数据提交
-    modifySimulationSchemeUrl: schemeIP+"/simulationTactics/modifyScheme/",
+    modifySimulationSchemeUrl: schemeIP+"/scheme-flow-server/simulationTactics/modifyScheme/",
     //获取工作流-办结列表
     hisTaskUrl: workflowIP+"/hydrogen-duties-server/histask/",
     //获取工作流-待办列表
@@ -145,6 +149,7 @@ const ReqUrls = {
 
 //协调相关url
 const CollaborateUrl = {
+    baseUrl: CollaborateIP + "/hydrogen-flight-coordination-server/flight",
     //标记豁免、取消豁免
     exemptUrl : CollaborateIP + "/hydrogen-flight-coordination-server/flight",
     //入池、出池
--- src/utils/request-urls.js
+++ src/utils/request-urls.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-24 13:18:28
- * @LastEditTime: 2021-05-24 15:28:09
+ * @LastEditTime: 2021-05-26 10:41:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WN-ATOM\src\utils\request-urls.js
