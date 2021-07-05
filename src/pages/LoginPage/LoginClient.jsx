f6ec0667a67aaf3481854133f323fc7e8b761e32
liutianjiao <liutj@adcc.com.cn>
2021-07-05 16:50:25 +0800

    213

--- src/pages/LoginPage/LoginClient.jsx
+++ src/pages/LoginPage/LoginClient.jsx
@@ -3,7 +3,7 @@ import LoginPage from "./LoginPage";
 import "./LoginClient.scss";
 
 function LoginClient(props) {
-  alert("登录页面初始化");
+  //   alert("登录页面初始化");
   return (
     <Fragment>
       <LoginPage pageType="client"></LoginPage>
