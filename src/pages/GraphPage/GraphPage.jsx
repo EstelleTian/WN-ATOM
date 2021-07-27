727f62591b1c26b5d2ea3f61632e35c52b5b2a7d
liutianjiao <liutj@adcc.com.cn>
2021-07-27 10:31:55 +0800

    流程图文件夹

---
+++ src/pages/GraphPage/GraphPage.jsx
@@ -0,0 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-18 18:39:39
+ * @LastEditTime: 2021-07-27 09:45:50
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: GraphPage.jsx
+ */
+import React from "react";
+import "./GraphPage.scss";
+
+//方案流程图模块
+const GraphPage = (props) => {
+  return (
+    <Layout className="work_detail_layout">
+      <div>方案流程图</div>
+    </Layout>
+  );
+};
+
+export default GraphPage;
